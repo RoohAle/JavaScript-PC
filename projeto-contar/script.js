479d74a036262965df5bfe82278a26b0004a2f26
RoohAle <54424798+RoohAle@users.noreply.github.com>
2026-03-09 10:32:09 -0300

    inclusao css e inicio js

---
+++ projeto-contar/script.js
@@ -0,0 +1,16 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo - document.getElementById('txtp')
+    let res = document.getElementById(res)
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando'
+    }
+
+
+
+
+}
