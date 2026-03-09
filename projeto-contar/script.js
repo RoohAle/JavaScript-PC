fbb47e5d713c564e503ff986ac1a7f46e3ba6672
RoohAle <54424798+RoohAle@users.noreply.github.com>
2026-03-09 10:48:00 -0300

    finalizado

--- projeto-contar/script.js
+++ projeto-contar/script.js
@@ -1,16 +1,31 @@
 function contar() {
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
-    let passo - document.getElementById('txtp')
-    let res = document.getElementById(res)
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossível contar!'
+        res.innerHTML = 'Impossível contar!' //pipe é alt + 124
     } else {
-        res.innerHTML = 'Contando'
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo Inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem Crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        } else {
+            // Contagem regressiva
+            for(let c = i; c>= f; c-= p){
+                res.innerHTML += `${c} \u{1f449}` // contra barra alt+92
+            }
+        }
+        res.innerHTML += '\u{1f3C1}'
     }
-
-
-
-
 }
